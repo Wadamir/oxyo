879f40567d004651f95b6cdcb5a81851baaaa005
Wadamir <domus159@gmail.com>
2025-05-23 01:50:52 +0500

    fix callback popup

--- catalog/language/en-gb/extension/module/oxyo_callback.php
+++ catalog/language/en-gb/extension/module/oxyo_callback.php
@@ -1,13 +1,29 @@
 <?php
 // Heading
-$_['heading_title']         = 'Callback Request';
+$_['heading_title']                 = 'Callback Request';
 
-// Text
-$_['text_name']             = 'Your Name';
-$_['text_phone']            = 'Your Phone';
-$_['text_success_form']     = 'Thank you! Your message has been sent.';
+// Callback Form
+$_['text_callback_title']           = 'Request a Callback';
+$_['text_callback_name']            = 'Your Name';
+$_['text_callback_email']           = 'Your Email';
+$_['text_callback_phone']           = 'Your Phone';
+$_['text_callback_message']         = 'Your Message';
+$_['text_callback_captcha']         = 'Answer The Question Below';
+$_['text_callback_agree']           = 'I agree to the processing of personal data';
+$_['text_callback_submit']          = 'Send';
+$_['text_callback_cancel']          = 'Cancel';
+$_['text_callback_success']         = 'Your message has been successfully sent';
+$_['text_callback_error']           = 'Error sending message. Please try again later.';
 
-// Errors
-$_['error_name']            = 'Your name must be between 2 and 30 characters';
-$_['error_phone']           = 'Invalid or missing phone number';
-$_['error_agreement']       = 'You must agree to the processing of personal data';
+$_['text_success_form']             = 'Thank you! Your message was sent.';
+$_['text_close']                    = 'Close';
+
+$_['error_name']                    = 'Your name must be between 2 and 30 characters';
+$_['error_email']                   = 'Invalid or missing email address';
+$_['error_phone']                   = 'Invalid or missing phone number';
+$_['error_message']                 = 'Message is missing';
+$_['error_captcha']                 = 'The verification answer was wrong';
+
+// Spam protection
+$_['text_callback_surname']         = 'Your Surname';
+$_['text_callback_spam_success']    = 'Thank you for your message. We will contact you soon.';
--- catalog/language/ru-ru/extension/module/oxyo_callback.php
+++ catalog/language/ru-ru/extension/module/oxyo_callback.php
@@ -1,13 +1,29 @@
 <?php
 // Heading
-$_['heading_title']         = 'Обратный звонок';
+$_['heading_title']                 = 'Обратный звонок';
 
-// Text
-$_['text_name']             = 'Ваше имя';
-$_['text_phone']            = 'Ваш телефон';
-$_['text_success_form']     = 'Спасибо! Ваше сообщение отправлено.';
+// Callback Form
+$_['text_callback_title']           = 'Заказать обратный звонок';
+$_['text_callback_name']            = 'Ваше имя';
+$_['text_callback_email']           = 'Ваш email';
+$_['text_callback_phone']           = 'Ваш телефон';
+$_['text_callback_message']         = 'Ваше сообщение';
+$_['text_callback_captcha']         = 'Ответьте на вопрос ниже';
+$_['text_callback_agree']           = 'Я согласен на обработку персональных данных';
+$_['text_callback_submit']          = 'Отправить';
+$_['text_callback_cancel']          = 'Отменить';
+$_['text_callback_success']         = 'Ваше сообщение успешно отправлено';
+$_['text_callback_error']           = 'Ошибка отправки сообщения. Пожалуйста, попробуйте позже.';
 
-// Errors
-$_['error_name']            = 'Имя должно быть от 2 до 30 символов';
-$_['error_phone']           = 'Некорректный номер телефона';
-$_['error_agreement']       = 'Вы должны согласиться на обработку персональных данных';
+$_['text_success_form']             = 'Спасибо! Ваше сообщение отправлено.';
+$_['text_close']                    = 'Закрыть';
+
+$_['error_name']                    = 'Имя должно содержать от 2 до 30 символов';
+$_['error_email']                   = 'Некорректный или отсутствующий email';
+$_['error_phone']                   = 'Некорректный или отсутствующий телефон';
+$_['error_message']                 = 'Сообщение не указано';
+$_['error_captcha']                 = 'Неправильный ответ на проверочный вопрос';
+
+// Spam protection
+$_['text_callback_surname']         = 'Ваша фамилия';
+$_['text_callback_spam_success']    = 'Спасибо за ваше сообщение. Мы свяжемся с вами в ближайшее время.';
