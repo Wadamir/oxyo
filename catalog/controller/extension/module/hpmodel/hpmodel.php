3fcf5336dbc8e9c60de8039b63bc590c32708307
Wadamir <domus159@gmail.com>
2025-07-20 12:44:50 +0500

    check hpm

--- catalog/controller/extension/module/hpmodel/hpmodel.php
+++ catalog/controller/extension/module/hpmodel/hpmodel.php
@@ -48,12 +48,14 @@ class ControllerExtensionModuleHpmodelHpmodel extends Controller
         $data['groups'] = $result['groups'];
 
         foreach ($data['groups'] as $group) {
-            foreach ($group['value'] as $key => $value) {
-                print_r($key);
-                echo "<br />";
-                print_r($value);
-                echo "<br />";
-            }
+            print_r($group);
+            echo "<br />";
+            // foreach ($group['value'] as $key => $value) {
+            //     print_r($key);
+            //     echo "<br />";
+            //     print_r($value);
+            //     echo "<br />";
+            // }
         }
 
         if (!empty($type['setting']['hidden_if_next'])) {
