b27f0dc0293be9c74b9cbedd7c0dc23e4987524f
Wadamir <domus159@gmail.com>
2025-07-27 20:19:32 +0500

    fix buyoneclick

--- catalog/language/en-gb/extension/module/oxyo_buyoneclick.php
+++ catalog/language/en-gb/extension/module/oxyo_buyoneclick.php
@@ -1,28 +1,28 @@
 <?php
 // Heading
-$_['heading_title']                 = 'Buy in One Click';
+$_['heading_title']                     = 'Buy in One Click';
 
 // Buyoneclick Form
-$_['text_buyoneclick_title']           = 'Buy in One Click';
-$_['text_buyoneclick_name']            = 'Your Name';
-$_['text_buyoneclick_email']           = 'Your Email';
-$_['text_buyoneclick_phone']           = 'Your Phone';
-$_['text_buyoneclick_message']         = 'Your Message';
-$_['text_buyoneclick_captcha']         = 'Answer The Question Below';
-$_['text_buyoneclick_agree']           = 'I agree to the processing of personal data';
-$_['text_buyoneclick_submit']          = 'Send';
-$_['text_buyoneclick_cancel']          = 'Cancel';
-$_['text_buyoneclick_success']         = 'Your request has been successfully sent';
-$_['text_buyoneclick_error']           = 'Error sending request. Please try again later.';
+$_['text_buyoneclick_title']            = 'Buy in One Click';
+$_['text_buyoneclick_name']             = 'Your Name';
+$_['text_buyoneclick_email']            = 'Your Email';
+$_['text_buyoneclick_phone']            = 'Your Phone';
+$_['text_buyoneclick_message']          = 'Your Message';
+$_['text_buyoneclick_captcha']          = 'Answer The Question Below';
+$_['text_buyoneclick_agree']            = 'I agree to the processing of personal data';
+$_['text_buyoneclick_submit']           = 'Send';
+$_['text_buyoneclick_cancel']           = 'Cancel';
+$_['text_buyoneclick_success']          = 'Your request has been successfully sent';
+$_['text_buyoneclick_error']            = 'Error sending request. Please try again later.';
 
-$_['text_success_form']             = 'Thank you! Your request was sent.';
-$_['text_close']                    = 'Close';
+$_['text_success_form']                 = 'Thank you! Your request was sent.';
+$_['text_close']                        = 'Close';
 
-$_['error_name']                    = 'Your name must be between 2 and 30 characters';
-$_['error_email']                   = 'Invalid or missing email address';
-$_['error_phone']                   = 'Invalid or missing phone number';
-$_['error_message']                 = 'Message is missing';
-$_['error_captcha']                 = 'The verification answer was wrong';
+$_['error_name']                        = 'Your name must be between 2 and 30 characters';
+$_['error_email']                       = 'Invalid or missing email address';
+$_['error_phone']                       = 'Invalid or missing phone number';
+$_['error_message']                     = 'Message is missing';
+$_['error_captcha']                     = 'The verification answer was wrong';
 
 // Spam protection
 $_['text_buyoneclick_surname']         = 'Your Surname';
