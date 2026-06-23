f4f26089fb3efe754fbad3fb8bdd09a96f4819ed
Wadamir <domus159@gmail.com>
2026-06-23 11:30:40 +0500

    fix admin mail

--- catalog/controller/mail/order.php
+++ catalog/controller/mail/order.php
@@ -558,6 +558,7 @@ class ControllerMailOrder extends Controller {
 					'model'    => $order_product['model'],
 					'quantity' => $order_product['quantity'],
 					'option'   => $option_data,
+                    'price'    => html_entity_decode($this->currency->format($order_product['price'] + ($this->config->get('config_tax') ? $order_product['tax'] : 0), $order_info['currency_code'], $order_info['currency_value']), ENT_NOQUOTES, 'UTF-8'),
 					'total'    => html_entity_decode($this->currency->format($order_product['total'] + ($this->config->get('config_tax') ? ($order_product['tax'] * $order_product['quantity']) : 0), $order_info['currency_code'], $order_info['currency_value']), ENT_NOQUOTES, 'UTF-8')
 				);
 			}
